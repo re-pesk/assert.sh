a139ed458f546cc9f57c5a8568f67cb4155e193f
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2025-07-30 16:47:27 +0300

    Add test file compatible with dash and tests runner

---
+++ test/tests.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#####################################################################
+##
+## description:
+## Tests runner for assert extension
+##
+## author: Rėdas Peškaitis
+## ISO date: 2025-07-30
+##
+## license: MIT
+##
+#####################################################################
+
+set -o nounset
+
+DIR_TEST="$( cd "$( dirname "${0}" )" && pwd )"
+
+"$DIR_TEST/test_assert.bash"
+"$DIR_TEST/test_assert.dash"
