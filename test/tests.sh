e443bc47f587f81ebc7dadc6abeb788d9cbb8ab2
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2025-08-02 02:33:18 +0300

    Add test files for yash and osh

--- test/tests.sh
+++ test/tests.sh
@@ -16,7 +16,9 @@ set -o nounset
 
 DIR_TEST="$( cd "$( dirname "${0}" )" && pwd )"
 
-"$DIR_TEST/test_assert.bash"
 "$DIR_TEST/test_assert.dash"
+"$DIR_TEST/test_assert.yash"
+"$DIR_TEST/test_assert.bash"
 "$DIR_TEST/test_assert.zsh"
 "$DIR_TEST/test_assert.ksh"
+"$DIR_TEST/test_assert.osh"
